bab54fbca84619854f7df89342663fd01a59d59a
JimS-1987 <78493751+JimS-1987@users.noreply.github.com>
2021-03-29 12:57:36 +0200

    Add files via upload

---
+++ opdracht_17_javascript_1/index.js
@@ -0,0 +1,16 @@
+console.log("Hello Winc Academy");
+let firstName = 'Jim';
+console.log(firstName);
+let number= 4;
+console.log(4+4);
+let numbeName= "4";
+console.log("4"+"4");
+console.log(4-4);
+console.log(4*4);
+console.log(4/4);
+console.log(1%4);
+let myAge="33";
+console.log(myAge);
+
+
+
