66b1059c03f69a47c74ab00072b765b40b7c667a
JimS-1987 <78493751+JimS-1987@users.noreply.github.com>
2021-04-01 10:50:38 +0200

    Add files via upload

---
+++ opdracht_019_if_else/index.js
@@ -0,0 +1,31 @@
+const age = (18);
+
+if ( age > 17){ 
+
+    console.log("Welcome in!");
+
+}else{
+
+    console.log("Stay out!");
+}
+
+const isFemale = false;
+
+if (isFemale){
+
+    console.log("50% discount!")
+
+}else{
+    console.log ("We are not gender-racist only someone has to pay the bills!  ")
+}
+
+const driverStatus = "bob";
+
+if ("bob"){
+
+    console.log("Drive save!")
+
+}else{
+
+    console.log("Do not drive!!!")
+}
