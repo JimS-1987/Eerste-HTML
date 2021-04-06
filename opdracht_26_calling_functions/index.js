77fb5addd2b508d1604e5dea8ec1a21f600f5848
JimS-1987 <78493751+JimS-1987@users.noreply.github.com>
2021-04-06 16:16:02 +0200

    Add files via upload

---
+++ opdracht_26_calling_functions/index.js
@@ -0,0 +1,41 @@
+/*
+const theAge =function(age) {
+  console.log("the age is.."+ age)
+  let ageIs =(age >=18);
+  if (ageIs){
+  return console.log( "  true"); 
+}return console.log( "  false");
+}
+theAge(18);
+
+const greetsAge = function(age){
+  if (theAge >= 18){
+    return console.log( "Hello There")
+  } return console.log("Hey kiddo")
+}
+greetsAge(18)
+
+
+*/
+
+const vatCalculatorhigh = function (basePrice){
+
+
+    console.log (basePrice + basePrice * 0.21);
+    return vatCalculatorhigh;
+ };
+   vatCalculatorhigh(500)
+
+
+
+
+
+const vatAmount = function (basePrice){
+
+console.log (basePrice - basePrice * 0.21)
+
+return vatAmount;
+};
+vatAmount(151)
+
+
