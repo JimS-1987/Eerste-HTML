7ab9d528e54143942832563d01a2836b823c60dc
JimS-1987 <78493751+JimS-1987@users.noreply.github.com>
2021-04-08 11:46:15 +0200

    Add files via upload

---
+++ opdracht_28_colors/index.js
@@ -0,0 +1,29 @@
+let Colors = ["yellow", "blue", "red", "orange"];
+/*
+
+while (Colors === ("orange")) {
+
+  console.log("your color is not orange but " + ("yellow"));
+  console.log("your color is not orange but " + ("blue"));
+  console.log("your color is not orange but " + ("red"));
+
+  Colors++;
+}
+console.log("the color is " + ("orange"));
+
+for (Colors; Colors.length; Colors++) {
+
+
+
+  console.log("your color is not orange but " + ("yellow"));
+  console.log("your color is not orange but " + ("blue"));
+  console.log("your color is not orange but " + ("red"));
+}
+console.log("the color is " + ("orange"));
+
+*/
+
+
+
+
+Colors.forEach(element => console.log(("this is the color  ") + element));
