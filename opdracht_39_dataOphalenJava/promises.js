b23958f354d5678727823a4c38d170a58c408920
JimS-1987 <78493751+JimS-1987@users.noreply.github.com>
2021-06-08 15:09:03 +0200

    Add files via upload

---
+++ opdracht_39_dataOphalenJava/promises.js
@@ -0,0 +1,22 @@
+const posts = [
+
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post two', body: 'this is post two' }
+];
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+
+    }, 1000);
+}
+
+function createPost(post) {
+    return new Promise ((resolve ,reject))
+    setTimeout(() => {
+        posts.push(post);
+    }, 2000);
+}
---
+++ opdracht_39_dataOphalenJava/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+function huiswerkMaken(vak, callback) {
+    console.log("oke oke ik ga mijn " + vak + " huiswerk Maken")
+    setTimeout(() => {
+        klaarMetHuiswerk()
+
+
+
+    }, 1000);
+}
+huiswerkMaken("rekenen")
+
+
+function klaarMetHuiswerk() {
+    console.log("kijk Paps/Mams, ik ben klaar met mijn huiswerk")
+}
+huiswerkMaken("wiskunde")
+
