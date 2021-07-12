3a47e621bed4189adcc009aa0b9a2caccaa639f0
JimS-1987 <78493751+JimS-1987@users.noreply.github.com>
2021-07-12 16:56:36 +0200

    Delete find-needle.js

--- find-needle.js
+++
@@ -1,7 +0,0 @@
-const findNeedle = function (words, wordToBeFound) {
-    // Schconst getWordLengths = function (someWords) {
-    return words.indexOf(wordToBeFound)
-
-};
-
-module.exports = findNeedle;
